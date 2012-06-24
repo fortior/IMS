a663a5a805d439cc124210ec4ca9b42435ac9739
ShunnarMeng <sdjtu502@163.com>
2012-06-25 00:34:44 +0800

    remove unittest

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -115,7 +115,7 @@ Kohana::modules(array(
 	'image'      => MODPATH.'image',      // Image manipulation
 	'minion'     => MODPATH.'minion',     // CLI Tasks
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	'unittest'   => MODPATH.'unittest',   // Unit testing
+// 	'unittest'   => MODPATH.'unittest',   // Unit testing
 	'userguide'  => MODPATH.'userguide',  // User guide and API documentation	
 	'das'  => MODPATH.'das',  // Data Acquisition System	
 	'api'  => MODPATH.'api',  // export api data 	
